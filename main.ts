a34f9529065cb42ae36b2c3672ccf5910104c7cd
GÜNAY GÜZEL <102692490+guguz58@users.noreply.github.com>
2026-03-26 14:38:17 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,64 +1,49 @@
-let strip = neopixel.create(DigitalPin.P12, 36, NeoPixelMode.RGB)
-basic.showLeds(`
-    . . . . .
-    # . . . .
-    # . . . .
-    . # . . .
-    . . # # .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . #
-    . . . . #
-    . . . # .
-    . # # . .
-    `)
-basic.showIcon(IconNames.Happy)
+input.onButtonPressed(Button.A, function () {
+    pins.servoWritePin(AnalogPin.P8, 45)
+    basic.pause(2000)
+    pins.servoWritePin(AnalogPin.P8, 79)
+    basic.pause(2000)
+    pins.servoWritePin(AnalogPin.P8, 110)
+    basic.pause(2000)
+})
+input.onButtonPressed(Button.B, function () {
+    kalibre = kalibre + 1
+    if (kalibre == 0) {
+        pins.servoWritePin(AnalogPin.P8, 45)
+    } else if (kalibre == 1) {
+        pins.servoWritePin(AnalogPin.P8, 79)
+    } else if (kalibre == 2) {
+        pins.servoWritePin(AnalogPin.P8, 110)
+    } else {
+        pins.servoWritePin(AnalogPin.P8, 45)
+    }
+})
+let kalibre = 0
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.OBJECTCLASSIFICATION)
+kalibre = 0
 basic.forever(function () {
-    strip.showRainbow(1, 36)
     huskylens.request()
     if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        huskylens.writeName(1, "cam")
-        huskylens.writeOSD("cam", 150, 30)
+        huskylens.writeName(1, "mutlu")
+        huskylens.writeOSD("mutlu", 150, 30)
         huskylens.clearOSD()
-        pins.digitalWritePin(DigitalPin.P13, 1)
-        strip.clear()
-        strip.showColor(neopixel.colors(NeoPixelColors.Green))
-        pins.servoWritePin(AnalogPin.P7, 0)
-        basic.pause(5000)
-        pins.digitalWritePin(DigitalPin.P13, 0)
-        pins.servoWritePin(AnalogPin.P7, 180)
     }
     if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        huskylens.writeName(2, "kagit")
-        huskylens.writeOSD("kagit", 150, 30)
+        huskylens.writeName(2, "uykulu")
+        huskylens.writeOSD("uykulu", 150, 30)
         huskylens.clearOSD()
-        pins.digitalWritePin(DigitalPin.P14, 1)
-        strip.clear()
-        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
-        pins.servoWritePin(AnalogPin.P8, 0)
-        basic.pause(5000)
-        pins.servoWritePin(AnalogPin.P8, 180)
-        pins.digitalWritePin(DigitalPin.P14, 0)
     }
     if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        huskylens.writeName(3, "pet")
-        huskylens.writeOSD("pet sise", 150, 30)
+        huskylens.writeName(3, "Mutsuz")
+        huskylens.writeOSD("Mutsuz", 150, 30)
         huskylens.clearOSD()
-        pins.digitalWritePin(DigitalPin.P15, 1)
-        strip.clear()
-        strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
-        pins.servoWritePin(AnalogPin.P9, 0)
-        basic.pause(5000)
-        pins.servoWritePin(AnalogPin.P9, 180)
-        pins.digitalWritePin(DigitalPin.P15, 0)
     }
-    if (huskylens.isLearned(4)) {
-        strip.clear()
-        huskylens.writeName(4, "atik yok")
-        huskylens.writeOSD("atik yok", 150, 30)
-        huskylens.clearOSD()
+    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        basic.showIcon(IconNames.Happy)
+    } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        basic.showIcon(IconNames.Asleep)
+    } else if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        basic.showIcon(IconNames.Sad)
     }
 })
